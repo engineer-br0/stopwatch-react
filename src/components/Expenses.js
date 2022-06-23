1b2138c10f4f3ee5774d954ad1fc7b4921dff6ed
engineer-br0 <106507655+engineer-br0@users.noreply.github.com>
2022-06-23 10:34:47 +0530

    multiple component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,12 +1,12 @@
 import './Expenses.css'
-
+import ExpensesTitle from './ExpensesTitle'
 function Expenses(props){
     return(
         <div className='todo'>
         
            <div className='items'>
                 <div className='date'>{props.date.toDateString()}</div>
-                <div className='des'>  {props.title} </div>
+                <ExpensesTitle title={props.title} />
                 <div className='pro'>  {props.pro}</div>
            </div>
         </div>
---
+++ src/components/ExpensesTitle.js
@@ -0,0 +1,8 @@
+
+function ExpensesTitle(props){
+    return(
+        <div>{props.title}</div>
+    );
+}
+
+export default ExpensesTitle;
